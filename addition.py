4478120912370b00e1ae440b24ee8d18e9108760
Charlotte333333 <112644693+Charlotte333333@users.noreply.github.com>
2022-10-11 12:09:40 -0400

    chase

--- addition.py
+++ addition.py
@@ -1,5 +1,13 @@
 # Write your addition function in the space below...
 
+def add_two(num1 , num2):
+    total = num1 + num2
+    print("The sum of", num1, "and", num2, "is", total)
+
+add_two(-1009, 1001)
+
+
+
 
 # Do not change anything below these lines
 def main():
--- egg.py
+++ egg.py
@@ -1,5 +1,16 @@
 # charlotte
 
+def top():
+    print("--------")
+    print("/         \ ")
+    print("\        /")
+def bottom():
+    print("---------")
+    print("/         \ ")
+    print("\         /")
+
+def side():
+    print(5)
 
 def main():
     # all of your function calls should go here. Remove the word "pass" before adding function calls.
