bed678ca2d70c367262663bc7dd8d63723b9740b
Charlotte333333 <112644693+Charlotte333333@users.noreply.github.com>
2022-10-20 19:52:19 -0400

    HHh

--- assignment_three.py
+++ assignment_three.py
@@ -1,7 +1,29 @@
-def calculaterectangle():
-   areaOfRectangle = (length*width)
-   return
+#CharlotteMoore 10/19/22 (the purpose of this prom is to calculate the area of any given prism)
 
+
+print("Answer the questions  asked by the machine to calculate the area of a prism.")
+
+
+def width():
+   width = int(input("What is the rectangles width?"))
+   return width
+
+
+
+def length():
+   length = int(input("What is the rectangles length?"))
+   return length
+
+
+def height():
+   height = int(input("what is the rectangles height?"))
+   return height
+
+
+
+def calculaterectangle(length, width):
+   areaOfRectangle = (length*width)
+   return areaOfRectangle
 
 
 def totalsurface(length, width, height):
@@ -15,7 +37,10 @@ def totalsurface(length, width, height):
    print(total)
    return
 
-totalsurface(4, 3, 9)
+length= length()
+width= width()
+height= height()
+totalsurface(length, width, height)
 
 
 
