a7468790461d0679ba4acc79253571fbc885a4de
Charlotte333333 <112644693+Charlotte333333@users.noreply.github.com>
2022-10-28 14:05:08 -0400

    HHh

--- assignment_three.py
+++ assignment_three.py
@@ -5,28 +5,32 @@ print("Answer the questions  asked by the machine to calculate the area of a pri
 
 
 def width():
-   width = int(input("What is the rectangles width?"))
-   return width
+  """This function collects the width of a rectangle"""
+  width = int(input("What is the rectangles width?"))
+  return width
 
 
 
 def length():
+   """This function collects the length of a rectangle"""
    length = int(input("What is the rectangles length?"))
    return length
 
 
 def height():
+   """This function collects the height of a rectangle"""
    height = int(input("what is the rectangles height?"))
    return height
 
 
-
 def calculaterectangle(length, width):
-   areaOfRectangle = (length*width)
+   """This function calculates the area of a rectangle"""
+   areaOfRectangle = (length * width)
    return areaOfRectangle
 
 
 def totalsurface(length, width, height):
+   """This function collects the length width and height for a prism and then prints the totaln surface of a prism"""
    top = int(calculaterectangle(length, width))
    bottom = int(calculaterectangle(length, width))
    leftSide = int(calculaterectangle(height, width))
@@ -34,13 +38,17 @@ def totalsurface(length, width, height):
    front = int(calculaterectangle(height, length))
    back = int(calculaterectangle(height, length))
    total = int((top+bottom+back+leftSide+rightSide+front))
-   print(total)
+   print("The total surface area is", total)
    return
 
-length= length()
-width= width()
-height= height()
-totalsurface(length, width, height)
+def main():
+   """This function takes the length, width, and height for the final calculation"""
+   l = length()
+   w = width()
+   h = height()
+   totalsurface(l, w, h)
+
+main()
 
 
 
---
+++ testing.py
@@ -0,0 +1,6 @@
+def calculaterectangle(length, width):
+   """This function calculates the area of a rectangle"""
+   areaOfRectangle = (length * width)
+   return areaOfRectangle
+
+print(calculaterectangle(8,9))
